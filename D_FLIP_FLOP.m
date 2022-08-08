9d87df97958884774763cee3c27b0d6264e0153e
Tanishq Sharma <tanishqsharma04@gmail.com>
2022-08-08 18:24:44 +0930

    Add files via upload

---
+++ D_FLIP_FLOP.m
@@ -0,0 +1,12 @@
+Clk = [1 0 0 1 0 0 1];
+D = [1 1 0 0 0 1 1];
+for index=1:length(Clk)
+        if Clk(index)==1
+            Q(index) = D(index);
+        else
+            Q(index) = Q(index-1);
+        end
+end
+disp('       D Flip-Flop');
+disp('     Clk   D    O/P');
+disp([Clk' D' Q']);
